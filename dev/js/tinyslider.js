eb9a046845f11657128aad59d87fc6562364d645
eugeniekwon <eugenie.kwon@gmail.com>
2020-07-10 05:24:06 +0900

    index carousel

--- dev/js/tinyslider.js
+++ dev/js/tinyslider.js
@@ -1,42 +1,45 @@
 console.log("please work");
 
-tns({
-  container: '.my-slider',
-  items: 1,
-  gutter: 20,
-  navPosition: "bottom",
-  controlsPosition: "botton",
-  controlsText: ["BACK", "NEXT"],
-  startIndex: 2,
-  
-  "responsive": {
-    "350": {
-      edgePadding: 20,
-      gutter: 20,
-      "items": 1
-    },
+if ($('.my-slider').length > 0) {
+  tns({
+    container: '.my-slider',
+    items: 1,
+    gutter: 20,
+    navPosition: "bottom",
+    controlsPosition: "bottom",
+    controlsText: ["BACK", "NEXT"],
+    startIndex: 2,
 
-    "768": {
-      edgePadding: 50,
-      gutter: 20,
-      "items": 1
-    },
+    "responsive": {
+      "350": {
+        edgePadding: 20,
+        gutter: 20
+        //"items": 1
+      },
 
-    "1025": {
-      edgePadding: 60,
-      gutter: 20,
-      "items": 1
+      "768": {
+        edgePadding: 50,
+        gutter: 20
+        //"items": 1
+      },
+
+      "1025": {
+        edgePadding: 60,
+        gutter: 20
+        //"items": 1
+      }
     }
-  }
-});
+  });
+}
 
+if ($('.index-slider').length > 0) {
+  tns({
+    container: '.index-slider',
+    items: 1,
+    navPosition: "bottom",
+    controlsPosition: "bottom",
+    startIndex: 2,
+    autoplay: true
 
-tns({
-  container: '.my-slider-2',
-  items: 1,
-  navPosition: "bottom",
-  controlsPosition: "botton",
-  startIndex: 2,
-  autoplay: true
-
-});
+  });
+}
