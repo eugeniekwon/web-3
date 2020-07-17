f8e20bb47e300f1f99ea143e5b0cb1d4ca2a80d8
eugeniekwon <eugenie.kwon@gmail.com>
2020-07-18 03:40:36 +0900

    advanced features of form (css and js)

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,6 +2,20 @@
 // @codekit-prepend "tinyslider.js";
 
 $( document ).ready(function() {
-    console.log( "ready!!!" );
+    
+
+    $("#show-form-button").on("click", function(){
+        // console.log("button here");
+
+        // hide contact-button-section
+        $("#contact-button-section").hide();
+
+        // show form-container
+        $("#form-container").show();
+
+    });
+
+
+
 });
 
